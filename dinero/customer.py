eab66fd6697a91940b7901622e0479b5b91d6e90
Colin Thomas-Arnold <colin@fusionbox.com>
2012-02-21 17:36:33 -0700

    refactored __init__ into seperate modules - what do we think about this style versus 'big __init__'?

---
+++ dinero/customer.py
@@ -0,0 +1,40 @@
+from dinero import exceptions
+from dinero.configure import get_gateway
+
+
+class Customer(object):
+    """
+    A Customer resource. `Customer.create` uses the gateway to create a
+    customer.  You can use this Customer object in calls to
+    `Transaction.create`.
+    """
+
+    @classmethod
+    def create(cls, gateway_name=None, **kwargs):
+        gateway = get_gateway(gateway_name)
+        resp = gateway.create_customer(kwargs)
+        return cls(gateway_name=gateway.name, **resp)
+
+    @classmethod
+    def retrieve(cls, customer_id, gateway_name=None):
+        gateway = get_gateway(gateway_name)
+        resp = gateway.retrieve_customer(customer_id)
+        return cls(gateway_name=gateway.name, **resp)
+
+    def __init__(self, gateway_name, customer_id, **kwargs):
+        self.gateway_name = gateway_name
+        self.customer_id = customer_id
+        self.data = kwargs
+
+    def to_dict(self):
+        return vars(self)
+
+    @classmethod
+    def from_dict(cls, dict):
+        return cls(dict['gateway_name'],
+                   dict['customer_id'],
+                   **dict['data']
+                   )
+
+    def __repr__(self):
+        return "Customer({gateway_name!r}, {customer_id!r}, **{data!r})".format(**self.to_dict())
---
+++ dinero/transaction.py
@@ -0,0 +1,55 @@
+from dinero import exceptions
+from dinero.configure import get_gateway
+
+
+class Transaction(object):
+    """
+    A Transaction resource. `Transaction.create` uses the gateway to charge a
+    card, and returns an object for future manipulations of the transaction,
+    like refunding it.
+    """
+
+    @classmethod
+    def create(cls, price, gateway_name=None, **kwargs):
+        gateway = get_gateway(gateway_name)
+        resp = gateway.charge(price, kwargs)
+        return cls(gateway_name=gateway.name, **resp)
+
+    @classmethod
+    def retrieve(cls, transaction_id, gateway_name=None):
+        gateway = get_gateway(gateway_name)
+        resp = gateway.retrieve(transaction_id)
+        return cls(gateway_name=gateway.name, **resp)
+
+    def __init__(self, gateway_name, price, transaction_id, **kwargs):
+        self.gateway_name = gateway_name
+        self.price = price
+        self.transaction_id = transaction_id
+        self.data = kwargs
+
+    def refund(self, amount=None):
+        gateway = get_gateway(self.gateway_name)
+
+        try:
+            return gateway.refund(self, amount or self.price)
+        except exceptions.PaymentException:
+            if amount is None or amount == self.price:
+                return gateway.void(self)
+            else:
+                raise exceptions.PaymentException("You cannot refund a "
+                        "transaction that hasn't been settled unless you "
+                        "refund it for the full amount.")
+
+    def to_dict(self):
+        return vars(self)
+
+    @classmethod
+    def from_dict(cls, dict):
+        return cls(dict['gateway_name'],
+                   dict['price'],
+                   dict['transaction_id'],
+                   **dict['data']
+                   )
+
+    def __repr__(self):
+        return "Transaction({gateway_name!r}, {price!r}, {transaction_id!r}, **{data!r})".format(**self.to_dict())
