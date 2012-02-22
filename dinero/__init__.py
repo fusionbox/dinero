a459cc8da7f0a6676aa9e74175dbf9eaef772e04
Colin Thomas-Arnold <colin@fusionbox.com>
2012-02-22 12:24:23 -0700

    added gateways to __init__

--- dinero/__init__.py
+++ dinero/__init__.py
@@ -2,3 +2,4 @@ from dinero.exceptions import *
 from dinero.transaction import Transaction
 from dinero.customer import Customer
 from dinero.configure import configure
+from dinero.gateways import *
