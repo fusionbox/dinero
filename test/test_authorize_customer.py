0548927c9679294aa2db126d5c070557e066fb5d
Colin Thomas-Arnold <colin@fusionbox.com>
2012-02-23 14:24:36 -0700

    moving tests around

--- test/test_authorize_customer.py
+++ test/test_authorize_customer.py
@@ -45,6 +45,18 @@ def test_create_delete_customer():
     customer.delete()
 
 
+def test_retrieve_nonexistant_customer():
+    try:
+        customer = dinero.Customer.retrieve(1234567890)
+        if customer:
+            customer.delete()
+            customer = dinero.Customer.retrieve(1234567890)
+    except CustomerNotFoundError:
+        pass
+    else:
+        assert False, "CustomerNotFoundError expected"
+
+
 def test_create_retrieve_delete_customer():
     options = {
         'email': 'someone@fusionbox.com',
@@ -103,13 +115,43 @@ def test_CRUD_customer():
     customer.delete()
 
 
-def test_retrieve_nonexistant_customer():
-    try:
-        customer = dinero.Customer.retrieve(1234567890)
-        if customer:
-            customer.delete()
-            customer = dinero.Customer.retrieve(1234567890)
-    except CustomerNotFoundError:
-        pass
-    else:
-        assert False, "CustomerNotFoundError expected"
+def test_CRUD_customer_with_number_change():
+    options = {
+        'email': 'someone@fusionbox.com',
+
+        'first_name': 'Joey',
+        'last_name': 'Shabadoo',
+        'company': 'Shabadoo, Inc.',
+        'phone': '000-000-0000',
+        'fax': '000-000-0001',
+        'address': '123 somewhere st',
+        'state': 'SW',
+        'city': 'somewhere',
+        'zip': '12345',
+        'country': 'US',
+
+        'number': '4' + '1' * 15,
+        'month': '12',
+        'year': '2012',
+    }
+    new_company = 'Joey Junior, Inc.'
+    new_number = '4' + '2' * 15
+    new_last_4_test = '2222'
+    new_year = '2012'
+    new_month = '12'
+
+    customer = dinero.Customer.create(**options)
+    customer_id = customer.customer_id
+
+    customer = dinero.Customer.retrieve(customer_id)
+    customer.company = new_company
+    customer.number = new_number
+    customer.year = new_year
+    customer.month = new_month
+    customer.save()
+
+    customer = dinero.Customer.retrieve(customer_id)
+    assert customer.company == new_company, 'Customer new_company is "%s" not "%s"' % (customer.company, new_company)
+    print customer
+    assert customer.last_4 == new_last_4_test, 'Customer new_last_4 is "%s" not "%s"' % (customer.last_4, new_last_4_test)
+    customer.delete()
